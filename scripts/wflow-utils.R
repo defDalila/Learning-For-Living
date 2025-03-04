cd9f594a5b7d3ee1a1257e11a2436f54ae41640e
defDalila <defdalila@outlook.com>
2025-03-04 15:30:41 -0300

    Initial

--- scripts/wflow-utils.R
+++ scripts/wflow-utils.R
@@ -3,10 +3,16 @@ wflow_build()
 
 wflow_view()
 
-status <- wflow_status()
+wflow_status()
 
 
-library(comprehenr)
 
 
 wflow_publish(c("Learning-For-Living.Rproj", "_workflowr.yml", "pages/", "scripts/", "source/"), "Initial Commit")
+
+
+wflow_use_github("defdalila")
+
+
+wflow_git_push()
+d
